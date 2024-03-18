9c8292524d8e8982280ab01c39e30946219333b9
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-03-18 11:26:25 +0000

    add assets to public folder and creating auth layout

---
+++ resources/views/components/auth/auth-footer.blade.php
@@ -0,0 +1,12 @@
+<!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+
+    <script src="{{asset('assets/vendor/global/global.min.js')}}"></script>
+	<script src="{{asset('assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js')}}"></script>
+    <script src="{{asset('assets/js/custom.min.js')}}"></script>
+    <script src="{{asset('assets/js/dlabnav-init.js')}}"></script>
+
+</body>
+</html>
---
+++ resources/views/components/auth/auth-header.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        {{-- <title>Login | School MS</title> --}}
+        @stack('title')
+
+        <!-- Fonts -->
+        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> --}}
+
+        <!-- Styles -->
+        <link href="{{asset('assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    </head>
+    <body class="vh-100">
