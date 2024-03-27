a13b7bcaeeaf02ed27a9655c494a3af49b8378a7
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-03-27 09:20:56 +0000

    Update auth-header.blade.php

--- resources/views/components/auth/auth-header.blade.php
+++ resources/views/components/auth/auth-header.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         {{-- <title>Login | School MS</title> --}}
         @stack('title')
@@ -13,7 +14,8 @@
 
         <!-- Styles -->
         <link href="{{asset('assets/vendor/bootstrap-select/dist/css/bootstrap-select.min.css')}}" rel="stylesheet">
-        {{-- <link href="{{asset('assets/vendor/jquery-smartwizard/dist/css/smart_wizard.min.css')}}" rel="stylesheet"> --}}
+        <!-- sweetalert -->
+        <link href= "{{asset('assets/vendor/sweetalert2/dist/sweetalert2.min.css')}}" rel="stylesheet">
         <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
     </head>
     <body class="vh-100">
