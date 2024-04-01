82edbe08cc5a49fd6db38e32510e7b9df0b5cbea
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-04-01 17:54:15 +0000

    Update LevelsInSelectInputBasedOnBranchJS.blade.php

--- resources/views/custom-functions/LevelsInSelectInputBasedOnBranchJS.blade.php
+++ resources/views/custom-functions/LevelsInSelectInputBasedOnBranchJS.blade.php
@@ -1,13 +1,13 @@
 <script>
     $(document).ready(()=>{
-        $("#new-student-form select[name=student_branch]").on('change', (e)=>{
+        $("#new-student-admission-form select[name=student_branch]").on('change', (e)=>{
             e.preventDefault()
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
-            let branch_id = $("#new-student-form select[name=student_branch]").val()
+            let branch_id = $("#new-student-admission-form select[name=student_branch]").val()
             $.ajax({
                 url:'{{route('getLevelsByBranchId')}}',
                 method:'GET',
@@ -15,7 +15,27 @@
                 data:{branch_id: branch_id},
                 success:(Response)=>{
                     // console.log(Response)
-                    $("#new-student-form select[name=student_level]").html(Response)
+                    $("#new-student-admission-form select[name=student_level]").html(Response)
+                }
+            })
+        })
+
+        $("#update-student-admission-form select[name=student_branch]").on('change', (e)=>{
+            e.preventDefault()
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            let branch_id = $("#update-student-admission-form select[name=student_branch]").val()
+            $.ajax({
+                url:'{{route('getLevelsByBranchId')}}',
+                method:'GET',
+                cache:false,
+                data:{branch_id: branch_id},
+                success:(Response)=>{
+                    // console.log(Response)
+                    $("#update-student-admission-form select[name=student_level]").html(Response)
                 }
             })
         })
