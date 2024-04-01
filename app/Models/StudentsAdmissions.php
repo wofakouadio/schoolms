0d6bd42572289c7f42b5f979e9f65f10f7f08f55
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-04-01 17:53:52 +0000

    Update StudentsAdmissions.php

--- app/Models/StudentsAdmissions.php
+++ app/Models/StudentsAdmissions.php
@@ -2,10 +2,68 @@
 
 namespace App\Models;
 
+use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class StudentsAdmissions extends Model
+class StudentsAdmissions extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, UUID;
+    use InteractsWithMedia;
+
+    protected $fillable = [
+        'student_firstname',
+        'student_othername',
+        'student_lastname',
+        'student_gender',
+        'student_dob',
+        'student_pob',
+        'student_branch',
+        'student_level',
+        'student_house',
+        'student_category',
+        'student_residency_type',
+        'student_guardian_name',
+        'student_guardian_contact',
+        'student_guardian_address',
+        'student_guardian_email',
+        'student_guardian_occupation',
+        'admission_status',
+        'school_id'
+    ];
+
+    public function school(){
+        return $this->hasOne(School::class,  'id','school_id');
+    }
+
+    public function branch(){
+        return $this->hasOne(Branch::class,  'id','student_branch');
+    }
+
+    public function level(){
+        return $this->hasOne(Level::class, 'id','student_level');
+    }
+
+    public function house(){
+        return $this->hasOne(House::class, 'id','student_house');
+    }
+
+    public function category(){
+        return $this->hasOne(Category::class,'id','student_category');
+    }
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('student_profile')
+            ->singleFile()
+            ->useDisk('media');
+
+        $this
+            ->addMediaCollection('student_guardian_id_card')
+            ->singleFile()
+            ->useDisk('media');
+    }
 }
