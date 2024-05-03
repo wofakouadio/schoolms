0cc403c506bbb23a9ac61dcc3823bbfd79127d83
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-03 19:57:02 +0000

    Update MockReportController.php

--- app/Http/Controllers/Admin/Report/Mock/MockReportController.php
+++ app/Http/Controllers/Admin/Report/Mock/MockReportController.php
@@ -73,22 +73,22 @@ class MockReportController extends Controller
                 ->where('branch_id', $studentData->student_branch)
                 ->get();
 
-            $output = "<table  class='display table-bordered' style='width:100%; align-self: center'>";
+            $output = '<table  class="display table-bordered" style="width:100%; align-self: center;">';
             $output .= '<tr>';
-            $output .= '<td width="20%" class="text-center">' . $schoolProfile . '</td>';
-            $output .= '<td width="60%">
+            $output .= '<td width="20%" class="text-center" style="border-bottom:0">' .$schoolProfile .'</td>';
+            $output .= '<td width="60%" style="border:0">
                                 <h2 class="text-center fw-bolder text-danger">' . $schoolData->school_name . '</h2>
                                 <h6 class="text-center">' . $schoolData->school_location . '</h6>
                                 <h6 class="text-center">' . $schoolData->school_email . ' / ' . $schoolData->school_phoneNumber . '</h6>
                                 <p class="text-center text-info">' . $studentData->branch->branch_name . ' Branch</p>
                             </td>';
-            $output .= '<td width="20%" class="text-center">' . $studentProfile . '</td>';
+            $output .= '<td width="20%" class="text-center" style="border: 0">' . $studentProfile . '</td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td colspan="3"><p class="text-center text-uppercase fw-light">Student Assessment Record</p></td>';
+            $output .= '<td colspan="3" style="font-size:14pt; border-top: 0; border-bottom:0"><p class="text-center text-uppercase fw-light">Student Assessment Record</p></td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td colspan="3"><p class="text-center text-primary text-uppercase fw-semibold">Mock '
+            $output .= '<td colspan="3" style="font-size:14pt; border-top: 0"><p class="text-center text-primary text-uppercase fw-semibold">Mock '
                 . $mockData->mock_type
                 . ' Performance Summary</p></td>';
             $output .= '</tr>';
@@ -99,34 +99,37 @@ class MockReportController extends Controller
             $output .= '<td colspan="3">';
             $output .= '<table class="table-bordered" width="100%">';
             $output .= '<tr>';
-            $output .= '<td width="20%"><p>ID</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_id . '</p></td>';
-            $output .= '<td width="20%"><p>Name</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_firstname . ' '
+            $output .= '<td width="20%" style="font-size:14pt; padding: 10px"><p>Student ID</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p>' . $studentData->student_id . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Name</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p>' . $studentData->student_firstname . ' '
                 . $studentData->student_othername . ' '
                 . $studentData->student_lastname . '</p></td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td width="20%"><p>Level</p></td>';
-            $output .= '<td width="30%"><p>' . $levelData->level_name . '</p></td>';
-            $output .= '<td width="20%"><p>Residency</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_residency_type . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Level</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p>' . $levelData->level_name . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Residency</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p>' . $studentData->student_residency_type . '</p></td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td width="20%"><p>House</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->house->house_name . '</p></td>';
-            $output .= '<td width="20%"><p>Category</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->category->category_name . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt" style="font-size:14pt"><p>House</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt" style="font-size:14pt"><p>' .
+                $studentData->house->house_name . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt" style="font-size:14pt"><p>Category</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt" style="font-size:14pt"><p>' .
+                $studentData->category->category_name . '</p></td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td width="20%">Term</td>';
-            $output .= '<td width="30%">' . $termData->term_name . '</td>';
-            $output .= '<td width="10%">Academic Year</td>';
-            $output .= '<td width="10%">' . $termData->term_academic_year . '</td>';
+            $output .= '<td width="20%" style="font-size:14pt" style="font-size:14pt">Term</td>';
+            $output .= '<td width="30%" style="font-size:14pt" style="font-size:14pt">' . $termData->term_name . '</td>';
+            $output .= '<td width="10%" style="font-size:14pt" style="font-size:14pt">Academic Year</td>';
+            $output .= '<td width="10%" style="font-size:14pt" style="font-size:14pt">' .
+                $termData->term_academic_year . '</td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td width="50%" colspan="2">Total Score</td>';
-            $output .= '<td width="50%" colspan="2" class="text-center text-primary fw-bolder">'
+            $output .= '<td width="50%" style="font-size:14pt" style="font-size:14pt" colspan="2">Total Score</td>';
+            $output .= '<td width="50%" style="font-size:14pt" style="font-size:14pt" colspan="2" class="text-center text-primary fw-bolder">'
                 . $mockFirstEntry->total_score
                 . '</td>';
             $output .= '</tr>';
@@ -134,27 +137,27 @@ class MockReportController extends Controller
             $output .= '</td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td colspan="3">';
+            $output .= '<td colspan="3" style="font-size:14pt" style="font-size:14pt">';
             $output .= '<p class="fw-bolder text-uppercase text-center"><u>Details of result</u></p>';
             $output .= '</td>';
             $output .= '</tr>';
 
             $output .= '<tr>';
-            $output .= '<td width="33.33%" class="text-uppercase">Subject</td>';
-            $output .= '<td width="33.33%" class="text-uppercase">Score</td>';
-            $output .= '<td width="33.33%" class="text-uppercase">proficiency level</td>';
+            $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase">Subject</td>';
+            $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase">Score</td>';
+            $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase">proficiency level</td>';
             $output .= '</tr>';
 
             foreach ($mockBreakdown as $breakdown) {
                 $output .= '<tr>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">' . $breakdown->subject->subject_name . '</td>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">' . $breakdown->score . '</td>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">-</td>';
+                $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase fw-bolder">' . $breakdown->subject->subject_name . '</td>';
+                $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase fw-bolder">' . $breakdown->score . '</td>';
+                $output .= '<td width="33.33%" style="font-size:14pt" style="font-size:14pt" class="text-uppercase fw-bolder">-</td>';
                 $output .= '</tr>';
             }
 
             $output .= '<tr>';
-            $output .= '<td colspan="3">';
+            $output .= '<td colspan="3" style="font-size:14pt" style="font-size:14pt">';
             $output .= '<p class="fw-bolder text-uppercase text-center"><u>appraisal</u></p>';
             $output .= '</td>';
             $output .= '</tr>';
@@ -162,16 +165,19 @@ class MockReportController extends Controller
             $output .= '<td colspan="3">';
             $output .= '<table class="table-bordered" width="100%">';
             $output .= '<tr>';
-            $output .= '<td width="20%"><p>Conduct</p></td>';
-            $output .= '<td width="30%"><p class="text-uppercase">' . $mockFirstEntry->conduct . '</p></td>';
-            $output .= '<td width="20%"><p>Attitude</p></td>';
-            $output .= '<td width="30%"><p class="text-uppercase">' . $mockFirstEntry->attitude . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Conduct</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p class="text-uppercase">' . $mockFirstEntry->conduct
+                . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Attitude</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p class="text-uppercase">' . $mockFirstEntry->attitude
+                . '</p></td>';
             $output .= '</tr>';
             $output .= '<tr>';
-            $output .= '<td width="20%"><p>Interest</p></td>';
-            $output .= '<td width="30%"><p class="text-uppercase">' . $mockFirstEntry->interest . '</p></td>';
-            $output .= '<td width="20%"><p>General Remarks</p></td>';
-            $output .= '<td width="30%"><p class="text-uppercase">' . $mockFirstEntry->general_remarks . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>Interest</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt"><p class="text-uppercase">' . $mockFirstEntry->interest
+                . '</p></td>';
+            $output .= '<td width="20%" style="font-size:14pt"><p>General Remarks</p></td>';
+            $output .= '<td width="30%" style="font-size:14pt">' . $mockFirstEntry->general_remarks . '</p></td>';
             $output .= '</tr>';
             $output .= '</table>';
             $output .= '</td>';
