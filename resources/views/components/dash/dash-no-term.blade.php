d8b41b69bc897fa52f276d8e03c4bbde39e0f7b5
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-08 06:36:01 +0000

    Update dash-no-term.blade.php

--- resources/views/components/dash/dash-no-term.blade.php
+++ resources/views/components/dash/dash-no-term.blade.php
@@ -1,5 +1,10 @@
 <div class="row page-titles">
-    <div class="alert alert-danger solid alert-square "><strong>Error!</strong> You need to activate a term in the
-        portfolio main page NOW!!!
+{{--    <div class="alert alert-danger solid alert-square "><strong>Error!</strong> You need to activate a term in the--}}
+{{--        portfolio main page NOW!!!--}}
+{{--    </div>--}}
+    <div class="alert alert-danger solid alert-square ">
+        <strong>Error!</strong>
+        <a href="{{route('admin_school')}}" class="text-warning fw-bolder">Click here</a> to activate a term NOW!!!
     </div>
+
 </div>
