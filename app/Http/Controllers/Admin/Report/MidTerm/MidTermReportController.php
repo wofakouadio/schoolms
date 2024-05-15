0f56f2c2e0452365b9d3b5d7499b0deebdc59666
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-15 21:38:15 +0000

    Update MidTermReportController.php

--- app/Http/Controllers/Admin/Report/MidTerm/MidTermReportController.php
+++ app/Http/Controllers/Admin/Report/MidTerm/MidTermReportController.php
@@ -9,6 +9,7 @@ use App\Models\MidTermBreakdown;
 use App\Models\School;
 use App\Models\StudentsAdmissions;
 use App\Models\Term;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use function App\Helpers\TermAndAcademicYear;
@@ -18,14 +19,90 @@ class MidTermReportController extends Controller
     //index
     public function index(){
         $schoolTerm = TermAndAcademicYear();
-        return view('admin.dashboard.report.mid-term.index', compact('schoolTerm'));
+        $data = [] ?? null;
+        return view('admin.dashboard.report.mid-term.index', compact('schoolTerm', 'data'));
     }
 
     public function get_mid_term_report(Request $request){
         $mid_term = $request->mid_term;
         $level = $request->level;
         $student = $request->student;
-        $output = '';
+        $schoolTerm = TermAndAcademicYear();
+
+        //get mid-term first entry
+        $midTermFirst = MidTerm::where("mid_term", $mid_term)
+            ->where('level_id', $level)
+            ->where("student_id", $student)
+            ->where('school_id', Auth::guard('admin')->user()->school_id)
+            ->first();
+
+        if(!empty($midTermFirst)){
+
+            //get school data
+            $schoolData = School::where("id", Auth::guard('admin')->user()->school_id)->first();
+            //get school profile
+            if($schoolData->getMedia('school_logo')->count() == 0){
+                $schoolProfile = asset('assets/images/avatar/1.jpg');
+            }else{
+                $schoolProfile = $schoolData->getFirstMediaUrl('school_logo');
+            }
+            //get level details
+            $levelData = Level::where('id', $level)->first();
+            //get student details
+            $studentData = StudentsAdmissions::with('house')
+                ->with('category')
+                ->with('branch')
+                ->where("id", $student)->first();
+            //student profile
+            if($studentData->getMedia('student_profile')->count() == 0){
+                $studentProfile = asset('assets/images/profile/small/pic1.jpg');
+            }else{
+                $studentProfile = $studentData->getFirstMediaUrl('student_profile');
+            }
+
+            //get term details
+            $termData = Term::where('school_id', Auth::guard('admin')->user()->school_id)
+                ->where('id', $midTermFirst->term_id)
+                ->first();
+
+            //get mid-term breakdown entry
+            $midTermBreakdown = MidTermBreakdown::with('subject')
+                ->where('mid_term_student_id', $midTermFirst->id)
+                ->where('term_id', $midTermFirst->term_id)
+                ->where('student_id', $student)
+                ->where('school_id', Auth::guard('admin')->user()->school_id)
+                ->where('branch_id', $studentData->student_branch)
+                ->get();
+
+            $data[] = [
+                'status' => 1,
+                'notice' => 'record found',
+                'midTermFirst' => $midTermFirst,
+                'levelData' => $levelData,
+                'studentData' => $studentData,
+                'schoolProfile' => $schoolProfile,
+                'midTermData' => $mid_term,
+                'studentProfile' => $studentProfile,
+                'midTermBreakdown' => $midTermBreakdown,
+                'schoolData' => $schoolData,
+                'termData' => $termData
+            ];
+
+        }else{
+            $data[] = [
+                'status' => 0,
+                'notice' => 'No record found'
+            ];
+        }
+
+        return view('admin.dashboard.report.mid-term.index', compact('schoolTerm', 'data'));
+    }
+
+    public function download_mid_term_report(Request $request){
+        $mid_term = $request->mid_term;
+        $level = $request->level;
+        $student = $request->student;
+        $schoolTerm = TermAndAcademicYear();
 
         //get mid-term first entry
         $midTermFirst = MidTerm::where("mid_term", $mid_term)
@@ -72,96 +149,29 @@ class MidTermReportController extends Controller
                 ->where('branch_id', $studentData->student_branch)
                 ->get();
 
-            $output = "<table  class='display table-bordered' style='width:100%; align-self: center'>";
-            $output .= '<tr>';
-            $output .= '<td width="20%" class="text-center">' . $schoolProfile . '</td>';
-            $output .= '<td width="60%">
-                                <h2 class="text-center fw-bolder text-danger">' . $schoolData->school_name . '</h2>
-                                <h6 class="text-center">' . $schoolData->school_location . '</h6>
-                                <h6 class="text-center">' . $schoolData->school_email . ' / ' . $schoolData->school_phoneNumber . '</h6>
-                                <p class="text-center text-info">' . $studentData->branch->branch_name . ' Branch</p>
-                            </td>';
-            $output .= '<td width="20%" class="text-center">' . $studentProfile . '</td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td colspan="3"><p class="text-center text-uppercase fw-light">Student Assessment Record</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td colspan="3"><p class="text-center text-primary text-uppercase fw-semibold">'
-                . $mid_term
-                . ' Mid-Term Performance Summary</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td colspan="3"><p class="text-center text-primary">=========================================================================================================</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td colspan="3">';
-            $output .= '<table class="table-bordered" width="100%">';
-            $output .= '<tr>';
-            $output .= '<td width="20%"><p>ID</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_id . '</p></td>';
-            $output .= '<td width="20%"><p>Name</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_firstname . ' '
-                . $studentData->student_othername . ' '
-                . $studentData->student_lastname . '</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td width="20%"><p>Level</p></td>';
-            $output .= '<td width="30%"><p>' . $levelData->level_name . '</p></td>';
-            $output .= '<td width="20%"><p>Residency</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->student_residency_type . '</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td width="20%"><p>House</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->house->house_name . '</p></td>';
-            $output .= '<td width="20%"><p>Category</p></td>';
-            $output .= '<td width="30%"><p>' . $studentData->category->category_name . '</p></td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td width="20%">Term</td>';
-            $output .= '<td width="30%">' . $termData->term_name . '</td>';
-            $output .= '<td width="10%">Academic Year</td>';
-            $output .= '<td width="10%">' . $termData->term_academic_year . '</td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td width="50%" colspan="2">Total Score</td>';
-            $output .= '<td width="50%" colspan="2" class="text-center text-primary fw-bolder">'
-                . $midTermFirst->total_score
-                . '</td>';
-            $output .= '</tr>';
-            $output .= '</table>';
-            $output .= '</td>';
-            $output .= '</tr>';
-            $output .= '<tr>';
-            $output .= '<td colspan="3">';
-            $output .= '<p class="fw-bolder text-uppercase text-center"><u>Details of result</u></p>';
-            $output .= '</td>';
-            $output .= '</tr>';
-
-            $output .= '<tr>';
-            $output .= '<td width="33.33%" class="text-uppercase">Subject</td>';
-            $output .= '<td width="33.33%" class="text-uppercase">Score</td>';
-            $output .= '<td width="33.33%" class="text-uppercase">proficiency level</td>';
-            $output .= '</tr>';
-
-            foreach ($midTermBreakdown as $breakdown){
-                $output .= '<tr>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">' . $breakdown->subject->subject_name . '</td>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">' . $breakdown->score . '</td>';
-                $output .= '<td width="33.33%" class="text-uppercase fw-bolder">-</td>';
-                $output .= '</tr>';
-            }
-
-            $output .= '</table>';
-            $output .= '</td>';
-            $output .= '</tr>';
-
-            $output .= "</table>";
+            $data[] = [
+                'status' => 1,
+                'notice' => 'record found',
+                'midTermFirst' => $midTermFirst,
+                'levelData' => $levelData,
+                'studentData' => $studentData,
+                'schoolProfile' => $schoolProfile,
+                'midTermData' => $mid_term,
+                'studentProfile' => $studentProfile,
+                'midTermBreakdown' => $midTermBreakdown,
+                'schoolData' => $schoolData,
+                'termData' => $termData
+            ];
 
         }else{
-            $output .= '<h4 class="fw-bolder text-uppercase text-danger">No record found</h4>';
+            $data[] = [
+                'status' => 0,
+                'notice' => 'No record found'
+            ];
         }
-
-        return $output;
+        $pdf = Pdf::loadView("admin.dashboard.report.mid-term.StudentMidTermReportDownload",compact('data'))
+            ->setOptions(['defaultFont' => 'sans-serif']);
+        return $pdf->stream($studentData->student_id.'_'.$studentData->student_firstname.'_'
+            .$studentData->student_lastname.'_MidTerm_Report.pdf');
     }
 }
