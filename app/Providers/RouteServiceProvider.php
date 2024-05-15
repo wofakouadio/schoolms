32816fc260125cc86e8f0c1d095ae2738be5277f
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-15 21:38:26 +0000

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,14 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware('web')
+                ->prefix('teacher')
+                ->group(base_path('routes/teacher.php'));
         });
     }
 }
