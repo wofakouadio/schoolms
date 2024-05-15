03cac83f32bedc6ba1c909d7e8006eabde644390
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-15 21:38:37 +0000

    Update jsScript.blade.php

--- resources/views/admin/dashboard/report/end-of-term/jsScript.blade.php
+++ resources/views/admin/dashboard/report/end-of-term/jsScript.blade.php
@@ -21,23 +21,23 @@
             })
         })
 
-        $("#end_of_term_report_form").on("submit", (e)=>{
-            e.preventDefault()
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            $.ajax({
-                url:'{{route('get_end_of_term_report')}}',
-                method:'GET',
-                cache: false,
-                data: $("#end_of_term_report_form").serialize(),
-                success:(Response)=>{
-                    console.log(Response)
-                    $("#end_of_term_report_display").html(Response)
-                }
-            })
-        })
+        {{--$("#end_of_term_report_form").on("submit", (e)=>{--}}
+        {{--    e.preventDefault()--}}
+        {{--    $.ajaxSetup({--}}
+        {{--        headers: {--}}
+        {{--            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
+        {{--        }--}}
+        {{--    });--}}
+        {{--    $.ajax({--}}
+        {{--        url:'{{route('get_end_of_term_report')}}',--}}
+        {{--        method:'GET',--}}
+        {{--        cache: false,--}}
+        {{--        data: $("#end_of_term_report_form").serialize(),--}}
+        {{--        success:(Response)=>{--}}
+        {{--            console.log(Response)--}}
+        {{--            $("#end_of_term_report_display").html(Response)--}}
+        {{--        }--}}
+        {{--    })--}}
+        {{--})--}}
     })
 </script>
