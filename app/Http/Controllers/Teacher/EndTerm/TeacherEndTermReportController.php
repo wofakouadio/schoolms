8120bec00e31d0d3799850002f3a1a4fc3de4ed0
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-15 21:38:22 +0000

    Update TeacherEndTermReportController.php

--- app/Http/Controllers/Teacher/EndTerm/TeacherEndTermReportController.php
+++ app/Http/Controllers/Teacher/EndTerm/TeacherEndTermReportController.php
@@ -9,6 +9,7 @@ use App\Models\Level;
 use App\Models\School;
 use App\Models\StudentsAdmissions;
 use App\Models\Term;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use function App\Helpers\TermAndAcademicYear;
@@ -22,7 +23,7 @@ class TeacherEndTermReportController extends Controller
         return view('teacher.dashboard.report.end-of-term.index', compact('schoolTerm', 'data'));
     }
 
-    public function get_end_of_term_report(Request $request){
+    public function preview_end_of_term_report(Request $request){
         $term = $request->term;
         $level = $request->level;
         $student = $request->student;
@@ -93,174 +94,77 @@ class TeacherEndTermReportController extends Controller
         return view('teacher.dashboard.report.end-of-term.index', compact('schoolTerm', 'data'));
     }
 
-//    public function get_end_of_term_report(Request $request){
-//        $term = $request->term;
-//        $level = $request->level;
-//        $student = $request->student;
-//        $output = '';
-////
-////        //get end of term first entry
-//        $endTermFirst = EndOfTerm::where("level_id", $level)
-//        ->where("term_id", $term)
-//        ->where("student_id", $student)
-//        ->where('school_id', Auth::guard('teacher')->user()->school_id)
-//        ->first();
-////
-////        if(!empty($endTermFirst)){
-////            //get school data
-//            $schoolData = School::where("id", Auth::guard('teacher')->user()->school_id)->first();
-//            //get school profile
-//            if($schoolData->getMedia('school_logo')->count() == 0){
-//                $schoolProfile = "<img src='". asset('assets/images/avatar/1.jpg') ."' class='rounded-circle' width=200>";
-//            }else{
-//                $schoolProfile = "<img src='". $schoolData->getFirstMediaUrl('school_logo') ."' class='rounded' width=200>";
-//            }
-////            //get level details
-//            $levelData = Level::where('id', $level)->first();
-////            //get term details
-//            $termData = Term::where('school_id', Auth::guard('teacher')->user()->school_id)
-//                ->where('id', $term)
-//                ->first();
-////            //get student details
-//            $studentData = StudentsAdmissions::with('house')
-//                ->with('category')
-//                ->with('branch')
-//                ->where("id", $student)->first();
-////            //student profile
-//            if($studentData->getMedia('student_profile')->count() == 0){
-//                $studentProfile = "<img src='". asset('assets/images/profile/small/pic1.jpg') ."' class='rounded-circle' width=200>";
-//            }else{
-//                $studentProfile = "<img src='". $studentData->getFirstMediaUrl('student_profile') ."' class='rounded' width=200>";
-//            }
-////
-////            //get end of term breakdown entry
-//            $endTermBreakdown = EndOfTermBreakdown::with('subject')
-//                ->where("end_term_student_id", $endTermFirst->id)
-//                ->where("term_id", $term)
-//                ->where("student_id", $student)
-//                ->where('school_id', Auth::guard('teacher')->user()->school_id)
-//                ->where('branch_id', $studentData->student_branch)
-//                ->get();
-//
-//            $output = "<table  class='display table-bordered' style='width:100%; align-self: center'>";
-//                $output .= '<tr>';
-//                    $output .= '<td width="20%" class="text-center">' . $schoolProfile . '</td>';
-//                    $output .= '<td width="60%">
-//                                        <h2 class="text-center fw-bolder text-danger">' . $schoolData->school_name . '</h2>
-//                                        <h6 class="text-center">' . $schoolData->school_location . '</h6>
-//                                        <h6 class="text-center">' . $schoolData->school_email . ' / ' . $schoolData->school_phoneNumber . '</h6>
-//                                        <p class="text-center text-info">' . $studentData->branch->branch_name . ' Branch</p>
-//                                    </td>';
-//                    $output .= '<td width="20%" class="text-center">' . $studentProfile . '</td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3"><p class="text-center text-uppercase fw-light">Student Assessment Record</p></td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3"><p class="text-center text-primary text-uppercase fw-semibold"> End of Term '. $termData->term_name . ' Performance Summary</p></td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3"><p class="text-center text-primary">=========================================================================================================</p></td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3">';
-//                        $output .= '<table class="table-bordered" width="100%">';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%"><p>ID</p></td>';
-//                                $output .= '<td width="30%"><p>' . $studentData->student_id . '</p></td>';
-//                                $output .= '<td width="20%"><p>Name</p></td>';
-//                                $output .= '<td width="30%"><p>' . $studentData->student_firstname . ' '. $studentData->student_othername . ' '. $studentData->student_lastname . '</p></td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%"><p>Level</p></td>';
-//                                $output .= '<td width="30%"><p>' . $levelData->level_name . '</p></td>';
-//                                $output .= '<td width="20%"><p>Residency</p></td>';
-//                                $output .= '<td width="30%"><p>' . $studentData->student_residency_type . '</p></td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%"><p>House</p></td>';
-//                                $output .= '<td width="30%"><p>' . $studentData->house->house_name . '</p></td>';
-//                                $output .= '<td width="20%"><p>Category</p></td>';
-//                                $output .= '<td width="30%"><p>' . $studentData->category->category_name . '</p></td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%">Term</td>';
-//                                $output .= '<td width="30%">' . $termData->term_name . '</td>';
-//                                $output .= '<td width="10%">Academic Year</td>';
-//                                $output .= '<td width="10%">' . $termData->term_academic_year . '</td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="50%" colspan="2">Total Score</td>';
-//                                $output .= '<td width="50%" colspan="2" class="text-center text-primary fw-bolder">'
-//                                . $endTermFirst->total_score
-//                                . '</td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%">Total Class Score</td>';
-//                                $output .= '<td width="30%">' . $endTermFirst->total_class_score . '</td>';
-//                                $output .= '<td width="20%">Total Exam Score</td>';
-//                                $output .= '<td width="30%">' . $endTermFirst->total_exam_score . '</td>';
-//                            $output .= '</tr>';
-//                        $output .= '</table>';
-//                    $output .= '</td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3">';
-//                        $output .= '<p class="fw-bolder text-uppercase text-center"><u>Details of result</u></p>';
-//                    $output .= '</td>';
-//                $output .= '</tr>';
-//
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3">';
-//                        $output .= '<table class="table-bordered" width="100%">';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="25%" class="text-uppercase">Subject</td>';
-//                                $output .= '<td width="25%" class="text-uppercase">Class Score</td>';
-//                                $output .= '<td width="25%" class="text-uppercase">Exam Score</td>';
-//                                $output .= '<td width="25%" class="text-uppercase">proficiency level</td>';
-//                            $output .= '</tr>';
-//                            foreach ($endTermBreakdown as $breakdown){
-//                                $output .= '<tr>';
-//                                    $output .= '<td width="25%" class="text-uppercase fw-bolder">' .
-//                                        $breakdown->subject->subject_name . '</td>';
-//                                    $output .= '<td width="25%" class="text-uppercase fw-bolder">' . $breakdown->class_score .
-//                                        '</td>';
-//                                    $output .= '<td width="25%" class="text-uppercase fw-bolder">' . $breakdown->exam_score .
-//                                        '</td>';
-//                                    $output .= '<td width="25%" class="text-uppercase fw-bolder">-</td>';
-//                                $output .= '</tr>';
-//                            }
-//                        $output .= '</table>';
-//                    $output .= '</td>';
-//                $output .= '</tr>';
-//
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3"><p class="fw-bolder text-uppercase text-center"><u>appraisal</u></p></td>';
-//                $output .= '</tr>';
-//                $output .= '<tr>';
-//                    $output .= '<td colspan="3">';
-//                        $output .= '<table class="table-bordered" width="100%">';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%"><p>Conduct</p></td>';
-//                                $output .= '<td width="30%"><p class="text-uppercase">' . $endTermFirst->conduct . '</p></td>';
-//                                $output .= '<td width="20%"><p>Attitude</p></td>';
-//                                $output .= '<td width="30%"><p class="text-uppercase">' . $endTermFirst->attitude . '</p></td>';
-//                            $output .= '</tr>';
-//                            $output .= '<tr>';
-//                                $output .= '<td width="20%"><p>Interest</p></td>';
-//                                $output .= '<td width="30%"><p class="text-uppercase">' . $endTermFirst->interest . '</p></td>';
-//                                $output .= '<td width="20%"><p>General Remarks</p></td>';
-//                                $output .= '<td width="30%"><p class="text-uppercase">' . $endTermFirst->general_remarks . '</p></td>';
-//                            $output .= '</tr>';
-//                        $output .= '</table>';
-//                    $output .= '</td>';
-//                $output .= '</tr>';
-//
-//            $output .= "</table>";
-//
-//        }else{
-//            $output .= '<h4 class="fw-bolder text-uppercase text-danger">No record found</h4>';
-//        }
-//        return $output;
-//    }
+    public function download_end_of_term_report(Request $request){
+        $term = $request->term;
+        $level = $request->level;
+        $student = $request->student;
+
+        //get end of term first entry
+        $endTermFirst = EndOfTerm::where("level_id", $level)
+            ->where("term_id", $term)
+            ->where("student_id", $student)
+            ->where('school_id', Auth::guard('teacher')->user()->school_id)
+            ->first();
+
+        if($endTermFirst){
+            //get school data
+            $schoolData = School::where("id", Auth::guard('teacher')->user()->school_id)->first();
+            //get school profile
+            if($schoolData->getMedia('school_logo')->count() == 0){
+                $schoolProfile = asset('assets/images/avatar/1.jpg');
+            }else{
+                $schoolProfile = $schoolData->getFirstMediaUrl('school_logo');
+            }
+            //get level details
+            $levelData = Level::where('id', $level)->first();
+            //get term details
+            $termData = Term::where('school_id', Auth::guard('teacher')->user()->school_id)
+                ->where('id', $term)
+                ->first();
+            //get student details
+            $studentData = StudentsAdmissions::with('house')
+                ->with('category')
+                ->with('branch')
+                ->where("id", $student)->first();
+            //student profile
+            if($studentData->getMedia('student_profile')->count() == 0){
+                $studentProfile = asset('assets/images/profile/small/pic1.jpg');
+            }else{
+                $studentProfile = $studentData->getFirstMediaUrl('student_profile');
+            }
+
+            //get end of term breakdown entry
+            $endTermBreakdown = EndOfTermBreakdown::with('subject')
+                ->where("end_term_student_id", $endTermFirst->id)
+                ->where("term_id", $term)
+                ->where("student_id", $student)
+                ->where('school_id', Auth::guard('teacher')->user()->school_id)
+                ->where('branch_id', $studentData->student_branch)
+                ->get();
+
+            $data[] = [
+                'status' => 1,
+                'notice' => 'record found',
+                'endTermFirst' => $endTermFirst,
+                'levelData' => $levelData,
+                'studentData' => $studentData,
+                'schoolProfile' => $schoolProfile,
+                'studentProfile' => $studentProfile,
+                'schoolData' => $schoolData,
+                'termData' => $termData,
+                'endTermBreakdown' => $endTermBreakdown
+            ];
+        }else{
+            $data[] = [
+                'status' => 0,
+                'notice' => 'No record found'
+            ];
+        }
+
+        $pdf = Pdf::loadView("teacher.dashboard.report.end-of-term.StudentEndTermReportDownload",compact('data'))
+            ->setOptions(['defaultFont' => 'sans-serif']);
+        return $pdf->stream($studentData->student_id.'_'.$studentData->student_firstname.'_'
+            .$studentData->student_lastname.'_End_of_Term_Report.pdf');
+    }
+
 }
