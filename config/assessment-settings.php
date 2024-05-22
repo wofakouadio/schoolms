37268fad71b9eb8e7137187ffb7a31a9ac8e8679
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-22 03:52:51 +0000

    Update assessment-settings.php

--- config/assessment-settings.php
+++ config/assessment-settings.php
@@ -2,5 +2,6 @@
 
 return [
     'class_percentage' => env('CLASS_PERCENTAGE', '50'),
-    'exam_percentage' => env('EXAM_PERCENTAGE', '50')
+    'exam_percentage' => env('EXAM_PERCENTAGE', '50'),
+    'class_assessment_size' => env('CLASS_ASSESSMENT_SIZE', '10')
 ];
