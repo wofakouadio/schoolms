50d72c417923f7d8928614477122f287e5508735
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-05-22 03:52:20 +0000

    Update SchoolController.php

--- app/Http/Controllers/Admin/School/SchoolController.php
+++ app/Http/Controllers/Admin/School/SchoolController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\School;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use App\Models\School;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -18,12 +19,16 @@ class SchoolController extends Controller
         $school_id = Auth::guard('admin')->user()->school_id;
         $schoolData = School::where('id', $school_id)->first();
         $schoolData->getMedia("school_logo")->first();
-//        dd($schoolData);
+
+        $adminData = Admin::where('school_id', $school_id)->first();
+        $adminData->getMedia("admin_profile")->first();
+//        dd($adminData);
         $schoolTerm = TermAndAcademicYear();
         return view('admin.dashboard.portfolio.index',
             [
                 'schoolData' => $schoolData,
-                'schoolTerm' => $schoolTerm
+                'schoolTerm' => $schoolTerm,
+                'adminData' => $adminData,
             ]
         );
     }
@@ -38,12 +43,6 @@ class SchoolController extends Controller
             'school_logo' => 'image|mimes:jpeg,jpg,png,gif'
         ]);
 
-        // if($request->hasFile('school_logo'))
-        //     // $logo = $request->file('school_logo')->store('school/logo', 'public');
-        //     $logo = $request->file('school_logo')->store('school/logo', 'public');
-        // else
-        //     $logo = $request->school_fetched_logo;
-
         DB::beginTransaction();
         try {
             $school = School::where('id', Auth::guard('admin')->user()->school_id)->update([
@@ -51,7 +50,6 @@ class SchoolController extends Controller
                 'school_location' => $request->school_location,
                 'school_email' => strtolower($request->school_email),
                 'school_phoneNumber' => $request->school_contact,
-                // 'school_logo' => $logo
             ]);
 
             if ($request->hasFile('school_logo')) {
