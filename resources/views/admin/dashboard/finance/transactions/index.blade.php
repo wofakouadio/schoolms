5a631b57697c19b7957af3ae01bc2f0d6d993a03
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-07-19 20:34:19 +0000

    Update index.blade.php

--- resources/views/admin/dashboard/finance/transactions/index.blade.php
+++ resources/views/admin/dashboard/finance/transactions/index.blade.php
@@ -101,23 +101,23 @@
                                                     <div class="row">
                                                         <div class="col-3">
                                                             <label>Invoice Number</label>
-                                                            <input value="{{ $value->invoice_id }}" name="" class="form-control solid mb-4" readonly>
+                                                            <input value="{{ $value->invoice_id }}" name="fc[{{ $key }}][invoice_number]" class="form-control solid mb-4" readonly>
                                                         </div>
                                                         <div class="col-2">
                                                             <label>Item</label>
-                                                            <input value="{{ $value->description }}" name="" class="form-control solid mb-4" readonly>
+                                                            <input value="{{ $value->description }}" name="fc[{{ $key }}][item]" class="form-control solid mb-4" readonly>
                                                         </div>
                                                         <div class="col-2">
                                                             <label>Amount Due</label>
-                                                            <input type="text" name="amount_due" class="form-control solid mb-4" readonly value="{{ $value->amount_due }}">
+                                                            <input type="text" name="fc[{{ $key }}][amount_due]" class="form-control solid mb-4" readonly value="{{ $value->amount_due }}">
                                                         </div>
                                                         <div class="col-2">
                                                             <label>Amount to allocate</label>
-                                                            <input type="number" name="amount_to_pay[{{ $key }}]" class="form-control solid mb-4 amount_to_pay" id="amount_to_pay">
+                                                            <input type="number" name="fc[{{ $key }}][amount_to_pay]" class="form-control solid mb-4 amount_to_pay" id="amount_to_pay">
                                                         </div>
                                                         <div class="col-3">
                                                             <label>Transaction ID/Reference</label>
-                                                            <input type="number" name="transaction_id[{{ $key }}]" class="form-control solid mb-4">
+                                                            <input type="number" name="fc[{{ $key }}][transaction_id]" class="form-control solid mb-4">
                                                         </div>
                                                     </div>
                                                 </li>
