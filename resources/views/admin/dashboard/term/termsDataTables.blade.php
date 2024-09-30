d596200c902ed8866f44c9f811bf095267137058
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-09-30 06:19:19 +0000

    Update termsDataTables.blade.php

--- resources/views/admin/dashboard/term/termsDataTables.blade.php
+++ resources/views/admin/dashboard/term/termsDataTables.blade.php
@@ -13,19 +13,19 @@
                 infoEmpty: "No records available",
                 infoFiltered: ""
             },
-            {{--processing: true,--}}
-            {{--serverSide: true,--}}
-            {{--ajax:{--}}
-            {{--    url:"{{route('termsTables')}}",--}}
-            {{--},--}}
-            {{--columns: [--}}
-            {{--    {data: 'name', name: 'name'},--}}
-            {{--    {data: 'opening_date', name: 'opening_date'},--}}
-            {{--    {data: 'closing_date', name: 'closing_date'},--}}
-            {{--    {data: 'academic_year', name: 'academic_year'},--}}
-            {{--    {data: 'is_active', name: 'is_active'},--}}
-            {{--    {data: 'action', name: 'action'},--}}
-            {{--],--}}
+            // {{--processing: true,--}}
+            // {{--serverSide: true,--}}
+            // {{--ajax:{--}}
+            // {{--    url:"{{route('termsTables')}}",--}}
+            // {{--},--}}
+            // {{--columns: [--}}
+            // {{--    {data: 'name', name: 'name'},--}}
+            // {{--    {data: 'opening_date', name: 'opening_date'},--}}
+            // {{--    {data: 'closing_date', name: 'closing_date'},--}}
+            // {{--    {data: 'academic_year', name: 'academic_year'},--}}
+            // {{--    {data: 'is_active', name: 'is_active'},--}}
+            // {{--    {data: 'action', name: 'action'},--}}
+            // {{--],--}}
             searching: true,
             paging: true,
             lengthChange: true,
