6ce3331e6c624302bde6a70c20103f7a03b91ea5
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-09-30 06:19:23 +0000

    Update ActiveTermInputBasedOnSchoolJS.blade.php

--- resources/views/custom-functions/admin/ActiveTermInputBasedOnSchoolJS.blade.php
+++ resources/views/custom-functions/admin/ActiveTermInputBasedOnSchoolJS.blade.php
@@ -7,25 +7,15 @@
                 }
             });
             $.ajax({
-                // @if(Auth::guard('admin')->check())
-                    url:'{{route('getActiveTermBySchoolID')}}',
-                // @elseif(Auth::guard('teacher')->check())
-                //     url:'{{route('getTeacherActiveTermBySchoolID')}}',
-                // @endif
+
+                url:'{{route("getActiveTermBySchoolID")}}',
                 method:'GET',
                 cache:false,
                 success:(Response)=>{
-                    // console.log(Response)
-                    // @if(Auth::guard('admin')->check())
-                        $("#new-end-term-setup-form").find("input[name=term_id]").val(Response['term_id']);
-                        $("#new-end-term-setup-form").find("input[name=term_name]").val(Response['term_name']);
-                        $("#new-end-term-setup-form").find("input[name=term_academic_year]").val
-                        (Response['academic_year_start']+'/'+Response['academic_year_end']);
-                    // @elseif(Auth::guard('teacher')->check())
-                    //     $("#new-end-term-setup-form").find("input[name=term_id]").val(Response['term_id']);
-                    //     $("#new-end-term-setup-form").find("input[name=term_name]").val(Response['term_name']);
-                    //     $("#new-end-term-setup-form").find("input[name=term_academic_year]").val(Response['academic_year_start']+'/'+Response['academic_year_end']);
-                    // @endif
+                    $("#new-end-term-setup-form").find("input[name=term_id]").val(Response['term_id']);
+                    $("#new-end-term-setup-form").find("input[name=term_name]").val(Response['term_name']);
+                    $("#new-end-term-setup-form").find("input[name=term_academic_year]").val
+                    (Response['academic_year_start']+'/'+Response['academic_year_end']);
                 }
             })
         }
