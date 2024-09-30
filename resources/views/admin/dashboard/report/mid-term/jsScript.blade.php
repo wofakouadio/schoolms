860bab48cc0145df801f40510b5b14b561df7590
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-09-30 06:19:09 +0000

    Update jsScript.blade.php

--- resources/views/admin/dashboard/report/mid-term/jsScript.blade.php
+++ resources/views/admin/dashboard/report/mid-term/jsScript.blade.php
@@ -11,7 +11,7 @@
                 }
             });
             $.ajax({
-                url:'{{route('getStudentsBasedOnLevel')}}',
+                url:"{{route('getStudentsBasedOnLevel')}}",
                 method:'GET',
                 cache:false,
                 data: {level_id:level_id},
@@ -20,24 +20,5 @@
                 }
             })
         })
-
-        {{--$("#mid_term_report_form").on("submit", (e)=>{--}}
-        {{--    e.preventDefault()--}}
-        {{--    $.ajaxSetup({--}}
-        {{--        headers: {--}}
-        {{--            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')--}}
-        {{--        }--}}
-        {{--    });--}}
-        {{--    $.ajax({--}}
-        {{--        url:'{{route('get_mid_term_report')}}',--}}
-        {{--        method:'GET',--}}
-        {{--        cache: false,--}}
-        {{--        data: $("#mid_term_report_form").serialize(),--}}
-        {{--        success:(Response)=>{--}}
-        {{--            console.log(Response)--}}
-        {{--            $("#mid_term_report_display").html(Response)--}}
-        {{--        }--}}
-        {{--    })--}}
-        {{--})--}}
     })
 </script>
