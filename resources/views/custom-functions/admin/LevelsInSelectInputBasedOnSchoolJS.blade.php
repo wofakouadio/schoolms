c47d41766d115e689c016c8f8c4eec7bbf934752
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-09-30 06:19:26 +0000

    Update LevelsInSelectInputBasedOnSchoolJS.blade.php

--- resources/views/custom-functions/admin/LevelsInSelectInputBasedOnSchoolJS.blade.php
+++ resources/views/custom-functions/admin/LevelsInSelectInputBasedOnSchoolJS.blade.php
@@ -1,14 +1,6 @@
 <script>
     $(document).ready(()=>{
 
-        // let route_url;
-
-        // @if (Auth::guard('admin')->check())
-        //     route_url = '{{ route('getLevelsBySchoolId') }}'
-        // @elseif(Auth::guard('teacher')->check())
-        //     route_url = '{{ route('getTeacherLevelsBySchoolId') }}'
-        // @endif
-
         const SchoolLevels = () =>{
             $.ajaxSetup({
                 headers: {
@@ -16,41 +8,25 @@
                 }
             });
             $.ajax({
-                // @if (Auth::guard('admin')->check())
-                    url: '{{ route('getLevelsBySchoolId') }}',
-                // @endif
-
-                // @if (Auth::guard('teacher')->check())
-                //     url: '{{ route('getTeacherLevelsBySchoolId') }}',
-                // @endif
+                url: '{{ route("getLevelsBySchoolId") }}',
                 method:'GET',
                 cache:false,
                 success:(Response)=>{
                     // console.log(Response)
-                    // @if(Auth::guard('admin')->check())
-                        $("#new-bill-form select[name=level]").html(Response)
-                        $("#update-bill-form select[name=level]").html(Response)
-                        $("#new-dl-form select[name=level]").html(Response)
-                        $("#update-dl-form select[name=level]").html(Response)
-                        $("#assign-subjects-to-mock-modal select[name=level]").html(Response)
-                        $("#new-student-mock-modal").find("select[name=level]").html(Response)
-                        $("#new-student-mock-with-bulk-upload-modal").find("select[name=level]").html(Response)
-                        $("#new-student-mid-term-modal").find("select[name=level]").html(Response)
-                        $("#new-end-term-setup-modal").find("select[name=level]").html(Response)
-                        $("#mock_report_form").find("select[name=level]").html(Response)
-                        $("#mid_term_report_form").find("select[name=level]").html(Response)
-                        $("#end_of_term_report_form").find("select[name=level]").html(Response)
-                        $("#new-assign-level-teacher-form").find("select[name=level]").html(Response)
-                        $("#new-level-assessment-form").find("select[name=level]").html(Response)
-                    // @elseif(Auth::guard('teacher')->check())
-                    //     $("#new-student-mock-modal").find("select[name=level]").html(Response)
-                    //     $("#new-level-assessment-form").find("select[name=level]").html(Response)
-                    //     $("#new-student-mid-term-modal").find("select[name=level]").html(Response)
-                    //     $("#new-end-term-setup-modal").find("select[name=level]").html(Response)
-                    //     $("#mock_report_form").find("select[name=level]").html(Response)
-                    //     $("#mid_term_report_form").find("select[name=level]").html(Response)
-                    //     $("#end_of_term_report_form").find("select[name=level]").html(Response)
-                    // @endif
+                    $("#new-bill-form select[name=level]").html(Response)
+                    $("#update-bill-form select[name=level]").html(Response)
+                    $("#new-dl-form select[name=level]").html(Response)
+                    $("#update-dl-form select[name=level]").html(Response)
+                    $("#assign-subjects-to-mock-modal select[name=level]").html(Response)
+                    $("#new-student-mock-modal").find("select[name=level]").html(Response)
+                    $("#new-student-mock-with-bulk-upload-modal").find("select[name=level]").html(Response)
+                    $("#new-student-mid-term-modal").find("select[name=level]").html(Response)
+                    $("#new-end-term-setup-modal").find("select[name=level]").html(Response)
+                    $("#mock_report_form").find("select[name=level]").html(Response)
+                    $("#mid_term_report_form").find("select[name=level]").html(Response)
+                    $("#end_of_term_report_form").find("select[name=level]").html(Response)
+                    $("#new-assign-level-teacher-form").find("select[name=level]").html(Response)
+                    $("#new-level-assessment-form").find("select[name=level]").html(Response)
                 }
             })
         }
