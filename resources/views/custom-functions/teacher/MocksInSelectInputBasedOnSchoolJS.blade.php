c3b1ba28be752f6a3100bc4fc05359407b286367
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-12 09:24:28 +0000

    Update MocksInSelectInputBasedOnSchoolJS.blade.php

--- resources/views/custom-functions/teacher/MocksInSelectInputBasedOnSchoolJS.blade.php
+++ resources/views/custom-functions/teacher/MocksInSelectInputBasedOnSchoolJS.blade.php
@@ -8,23 +8,12 @@
                 }
             });
             $.ajax({
-                @if(Auth::guard('admin')->check())
-                    url:'{{route('getMocksInSelectBasedOnSchool')}}',
-                @elseif(Auth::guard('teacher')->check())
-                    url:'{{route('getTeacherMocksInSelectBasedOnSchool')}}',
-                @endif
+                url:"{{route('getTeacherMocksInSelectBasedOnSchool')}}",
                 method:'GET',
                 cache:false,
                 success:(Response)=>{
-                    @if(Auth::guard('admin')->check())
-                        $("#assign-subjects-to-mock-modal").find("select[name=mock]").html(Response)
-                        $("#new-student-mock-modal").find("select[name=mock]").html(Response)
-                        $("#new-student-mock-with-bulk-upload-modal").find("select[name=mock]").html(Response)
-                        $("#mock_report_form").find("select[name=mock]").html(Response)
-                    @elseif(Auth::guard('teacher')->check())
-                        $("#new-student-mock-modal").find("select[name=mock]").html(Response)
-                        $("#mock_report_form").find("select[name=mock]").html(Response)
-                    @endif
+                    $("#new-student-mock-modal").find("select[name=mock]").html(Response)
+                    $("#mock_report_form").find("select[name=mock]").html(Response)
                 }
             })
         }
