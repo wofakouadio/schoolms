62c2afbcc2bf4628d9cae157cf2635fa46aabaaf
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-14 18:51:13 +0000

    Update SchoolDataJS.blade.php

--- resources/views/custom-functions/admin/SchoolDataJS.blade.php
+++ resources/views/custom-functions/admin/SchoolDataJS.blade.php
@@ -17,14 +17,15 @@
                             Response['school_data']['media'][0]['original_url'])
                     }else{
                         $(".nav-header .brand-logo").find('#school_logo').attr("src",
-                            '{{asset('storage/school/logo/profile.png')}}')
+                            "{{asset('storage/school/logo/profile.png')}}")
                     }
-                    // $("#new-teacher-form").find("input[name=teacher_present_school]").val(Response['school_data']['school_name']);
-                    // $(".nav-header .brand-title").html("<h6 " +
-                    //     "class='fw-bolder text-primary'>"+Response['school_data']['school_name']+"</h6>")
+                    $("#new-teacher-form").find("input[name=teacher_present_school]").val(Response['school_data']['school_name']);
+                    $(".nav-header .brand-title").html("<h6 " +
+                        "class='fw-bolder text-primary'>"+Response['school_data']['school_name']+"</h6>")
                 }
             })
         }
         SchoolData()
     })
 </script>
+assign-levels-to-teacher
