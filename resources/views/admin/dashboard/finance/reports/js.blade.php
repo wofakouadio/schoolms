a5ec813dd45997f742edd2070e628b58761ff139
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-22 09:45:58 +0000

    Create js.blade.php

---
+++ resources/views/admin/dashboard/finance/reports/js.blade.php
@@ -0,0 +1,26 @@
+<script>
+
+    $(document).ready(()=>{
+        $(".menu-alert").hide()
+
+        // $("#finance_get_student_data_form").on("submit", (e)=>{
+        //     e.preventDefault()
+        //     $.ajaxSetup({
+        //         headers: {
+        //             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        //         }
+        //     });
+        //     $.ajax({
+        //         method:'GET',
+        //         cache: false,
+        //         data: $("#get_student_data_form").serialize(),
+        //         success:(Response)=>{
+        //             console.log(Response)
+        //         }
+        //     })
+        // })
+
+
+    })
+
+</script>
