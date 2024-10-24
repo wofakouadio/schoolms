4f0ad0e10b472c5cb5b29e0d333c03d8c8af5a72
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-24 23:34:11 +0000

    Update DataTables.blade.php

--- resources/views/admin/dashboard/finance/feeding-fee/DataTables.blade.php
+++ resources/views/admin/dashboard/finance/feeding-fee/DataTables.blade.php
@@ -34,5 +34,39 @@
             aoStateSave: true
             // stateSave: true
         })
+
+        $("#feeding_fee_collection_table").DataTable({
+            language: {
+                paginate: {
+                    next: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+                    previous: '<i class="fa fa-angle-double-left" aria-hidden="true"></i>'
+                },
+                lengthMenu: "Display _MENU_",
+                zeroRecords: "Nothing found - sorry",
+                info: "Showing page _PAGE_ of _PAGES_",
+                infoEmpty: "No records available",
+                infoFiltered: ""
+            },
+            // processing: true,
+            // serverSide: true,
+            // ajax:{
+            //     url:"{{route('billsTables')}}",
+            // },
+            // columns: [
+            //     {data: 'academic_year', name: 'academic_year'},
+            //     {data: 'term', name: 'term'},
+            //     {data: 'level', name:'level'},
+            //     {data: 'amount', name: 'amount'},
+            //     {data: 'last_updated', name:'last_updated'},
+            //     {data: 'is_active', name: 'is_active'},
+            //     {data: 'action', name: 'action'},
+            // ],
+            searching: true,
+            paging: true,
+            lengthChange: true,
+            autoWidth: true,
+            aoStateSave: true
+            // stateSave: true
+        })
     })
 </script>
