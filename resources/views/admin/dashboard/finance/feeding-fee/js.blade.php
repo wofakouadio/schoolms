49f6e2c501f9334dc253d14b16622d04c32a7190
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-24 23:34:14 +0000

    Update js.blade.php

--- resources/views/admin/dashboard/finance/feeding-fee/js.blade.php
+++ resources/views/admin/dashboard/finance/feeding-fee/js.blade.php
@@ -48,6 +48,34 @@
             })
         })
 
+        // sync all values from feeding fee collection imput form
+        $("#btn_sync").on("click", (e)=>{
+            e.preventDefault()
+
+            let arv = (0);
+            let np = 0
+            let ndp = 0
+            let nc = 0
+            let ac = 0
+            let ap = 0
+            let ff = 0
+            
+            np = $("#feeding_fee_new_collection_form").find('input[name=number_of_presents]').val()
+            ndp = $("#feeding_fee_new_collection_form").find('input[name=number_of_do_not_pay]').val()
+            nc = $("#feeding_fee_new_collection_form").find('input[name=number_of_credits]').val()
+            ac = $("#feeding_fee_new_collection_form").find('input[name=arrears_clearance]').val()
+            ap = $("#feeding_fee_new_collection_form").find('input[name=advance_payment]').val()
+            ff = $("#feeding_fee_new_collection_form").find('input[name=feeding_fee]').val()
+
+            let a = parseFloat(np) - parseFloat(ndp) - parseFloat(nc)
+            let b = parseFloat(ac) + parseFloat(ap)
+
+            arv = (parseFloat(a) + parseFloat(b)) * parseFloat(ff)
+
+            $("#feeding_fee_new_collection_form").find('input[name=amount_realized]').val(arv)
+
+        })
+
 
     })
 
