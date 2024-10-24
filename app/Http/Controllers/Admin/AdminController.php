4caf746fea5dff92a9b2dfe01d3756b23f672b76
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2024-10-24 08:07:17 +0000

    Update AdminController.php

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\School;
+use App\Models\Teacher;
+use App\Models\StudentsAdmissions;
 use Illuminate\Support\Facades\Auth;
 use function App\Helpers\TermAndAcademicYear;
 
@@ -15,8 +17,24 @@ class AdminController extends Controller
         $schoolTerm = TermAndAcademicYear();
         $schoolData = School::where('id', Auth::guard('admin')->user()->school_id)->first();
         $schoolData->getMedia("school_logo")->first();
-        return view('admin.dashboard.index', ['schoolData' => $schoolData,
-            'schoolTerm' => $schoolTerm]);
+        // students
+        $studentsCount = StudentsAdmissions::where('school_id', Auth::guard('admin')->user()->school_id)->count();
+        $studentsBoys = StudentsAdmissions::where(['school_id' => Auth::guard('admin')->user()->school_id, 'student_gender' => 'Male'])->count();
+        $studentsGirls = StudentsAdmissions::where(['school_id' => Auth::guard('admin')->user()->school_id, 'student_gender' => 'Female'])->count();
+        // teachers
+        $teachersCount = Teacher::where('school_id', Auth::guard('admin')->user()->school_id)->count();
+        $teachersBoys = Teacher::where(['school_id' => Auth::guard('admin')->user()->school_id, 'teacher_gender' => 'Male'])->count();
+        $teachersGirls = Teacher::where(['school_id' => Auth::guard('admin')->user()->school_id, 'teacher_gender' => 'Female'])->count();
+        $dashRecords = [
+            'studentsCount' => $studentsCount,
+            'studentsBoys' => $studentsBoys,
+            'studentsGirls' => $studentsGirls,
+            'teachersCount' => $teachersCount,
+            'teachersBoys' => $teachersBoys,
+            'teachersGirls' => $teachersGirls
+        ];
+        // dd($dashRecords);
+        return view('admin.dashboard.index', compact('schoolData', 'schoolTerm', 'dashRecords'));
     }
 
     //login
